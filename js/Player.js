c5af65a47aa7753adfc02e0d17e3002f5ddf2740
Koovenn <educanaless@gmail.com>
2023-11-01 21:26:47 +0000

    Resuelto sin bonus

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,22 @@
-class Player {}
+class Player {
+    constructor(name, life, attack) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+
+    attackPlayer(player, diceValue) {
+        if (this.life > 0) {
+            player.life -= this.attack * diceValue;
+        }
+        return player.life;
+    }
+
+    isDead() {
+        if (this.life <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
